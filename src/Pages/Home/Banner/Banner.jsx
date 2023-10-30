afc062583bc0b8012d8db8b8d25d0d43a5e6cbf5
RajobAli <freelancerrajobali@gmail.com>
2023-10-30 20:27:42 +0600

    car-doctor frontent project

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,88 @@
+
+import image1 from '../../../assets/assets/icons/images/banner/1.jpg'
+import image2 from '../../../assets/assets/icons/images/banner/2.jpg'
+import image3 from '../../../assets/assets/icons/images/banner/3.jpg'
+import image4 from '../../../assets/assets/icons/images/banner/4.jpg'
+
+
+const Banner = () => {
+    return (
+        <div className="carousel w-full  h-[600px]">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img src={image1} className="w-full rounded-xl" />
+                <div className="absolute flex rounded-xl items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)] ">
+                    <div className='text-white space-y-7 pl-12 w-1/2'>
+                        <h2 className='text-6xl font-bold'>Affordable Price for car servicing</h2>
+                        <p>There are many variations of passages of available but the many majority have suffered alteration in some form</p>
+                        <div>
+                            <button className='btn btn-primary mr-5'>Discover More</button>
+                            <button className='btn btn-outline btn-success'>Latest Project</button>
+
+                        </div>
+                    </div>
+                </div>
+                <div className='absolute flex justify-end transform left-5 right-5 bottom-0 gap-5'>
+                    <a href="#slide4" className="btn btn-circle">❮</a>
+                    <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+
+            <div id="slide2" className="carousel-item relative w-full">
+                <img src={image2} className="w-full rounded-xl" />
+                <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)] ">
+                    <div className='text-white space-y-7 pl-12 w-1/2'>
+                        <h2 className='text-6xl font-bold'>Affordable Price for car servicing</h2>
+                        <p>There are many variations of passages of available but the many majority have suffered alteration in some form</p>
+                        <div>
+                            <button className='btn btn-primary mr-5'>Discover More</button>
+                            <button className='btn btn-outline btn-success'>Latest Project</button>
+
+                        </div>
+                    </div>
+                </div>
+                <div className='absolute flex justify-end transform left-5 right-5 bottom-0 gap-5'>
+                    <a href="#slide1" className="btn btn-circle">❮</a>
+                    <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+                <img src={image3} className="w-full rounded-xl" />
+                <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)] ">
+                    <div className='text-white space-y-7 pl-12 w-1/2'>
+                        <h2 className='text-6xl font-bold'>Affordable Price for car servicing</h2>
+                        <p>There are many variations of passages of available but the many majority have suffered alteration in some form</p>
+                        <div>
+                            <button className='btn btn-primary mr-5'>Discover More</button>
+                            <button className='btn btn-outline btn-success'>Latest Project</button>
+
+                        </div>
+                    </div>
+                </div>
+                <div className='absolute flex justify-end transform left-5 right-5 bottom-0 gap-5'>
+                    <a href="#slide2" className="btn btn-circle">❮</a>
+                    <a href="#slide4" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+                <img src={image4} className="w-full rounded-xl" />
+                <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)] ">
+                    <div className='text-white space-y-7 pl-12 w-1/2'>
+                        <h2 className='text-6xl font-bold'>Affordable Price for car servicing</h2>
+                        <p>There are many variations of passages of available but the many majority have suffered alteration in some form</p>
+                        <div>
+                            <button className='btn btn-primary mr-5'>Discover More</button>
+                            <button className='btn btn-outline btn-success'>Latest Project</button>
+
+                        </div>
+                    </div>
+                </div>
+                <div className='absolute flex justify-end transform left-5 right-5 bottom-0 gap-5'>
+                    <a href="#slide3" className="btn btn-circle">❮</a>
+                    <a href="#slide1" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
